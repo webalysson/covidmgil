e15aa2e9fdd9e7eeb6b45811d027fd6a4252e5bf
webalysson <webalysson@gmail.com>
2020-05-24 22:20:03 -0300

    Atualizado 24/05/2020

--- js/scripts.js
+++ js/scripts.js
@@ -4,14 +4,16 @@
         //https://raw.githubusercontent.com/webalysson/covidmgil/master/dados/notifications_mgil.csv
         d3.csv('https://docs.google.com/spreadsheets/d/1pmHJzLZsE-7wnoXAlnBXmp1vKRx7B8K8j5xsnkH9jWk/export?format=csv&gid=295642056')
         .then(function(data){
-            gerarGraficoArea(data);
+            gerarGraficoArea(data); //data.slice(-15)
             gerarGraficoBarras(data);
             gerarGraficoPizza(data);
-            document.getElementById('notificados').innerHTML=data[data.length-1].Notificados;
-            document.getElementById('suspeitos').innerHTML=data[data.length-1].Suspeitos;
-            document.getElementById('descartados').innerHTML=data[data.length-1].Descartados;
-            document.getElementById('confirmados').innerHTML=data[data.length-1].Confirmados;
-            ultimo_boletim = data[data.length-1];
+            gerarGraficoConfirmados(data.slice(-15));
+            ultimo_boletim = data[data.length-1]; //pegar o último registro de boletim cadastrado
+            document.getElementById('notificados').innerHTML = ultimo_boletim["Notificados"];
+            document.getElementById('suspeitos').innerHTML = ultimo_boletim["Suspeitos"];
+            document.getElementById('descartados').innerHTML = ultimo_boletim["Descartados"];
+            document.getElementById('confirmados').innerHTML = ultimo_boletim["Confirmados"];
+            //Inserir a informação na página inicial
             document.getElementById("info_atualizacao").innerHTML = "Último boletim: " + ultimo_boletim["Data"] + "/2020";
         });
 
